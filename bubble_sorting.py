3aa9ee7b92956c01ee5ba421214d7894b3a061c4
Emmanuel Ikpesu <emmanarutops2@gmail.com>
2022-07-23 19:39:43 +0100

    First Optimal Bubble Sorting

--- bubble_sorting.py
+++ bubble_sorting.py
@@ -1,10 +1,20 @@
 import time
+# sample list
 a = [3,2,1,5,4,7,9,8]
+count = len(a) - 1
 start = time.time()
-for n in a:
+
+# loop through list
+for n in range(count):
+# Review list current state
     print(a)
-    for m in range(len(a) - 1):
+# Compare neigboring values
+    for m in range(count):
+# Monitor number of comparison
+        print(m)
         if a[m+1] < a[m]:
             a[m+1], a[m] = a[m], a[m+1]
+# Reduce number of comparison since final n value is in the correct position
+    count -= 1
             
 print("new list is:",a,"\nIt took", time.time() - start)
