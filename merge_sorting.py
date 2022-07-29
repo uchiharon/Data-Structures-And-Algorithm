cb668d15a817ac9b9913ef155b1577bd9cbe95cc
Emmanuel Ikpesu <emmanarutops2@gmail.com>
2022-07-29 23:19:03 +0100

    fix: Complete Recursive spliting of array

--- merge_sorting.py
+++ merge_sorting.py
@@ -1,16 +1,26 @@
 value = [3,4,5,2,1,5]
 
+
+def merge_sort(value):
+    if len(value) > 1:
+        print(value)
+        mid = round(len(value)/2)
+        merge_sort(value[:mid])
+        merge_sort(value[mid:])
+    else:
+        print(value)
+        
+merge_sort(value)
+        
+'''
+
 for n in value:
     if len(n) > 1:
         for m in range(len(n)-1):
             if n[m] > n[m+1]:
                 n[m+1],n[m] = n[m],n[m+1]
-        
-
-
-def merge_sort(value):
-    sorter = []
-
+                
+                
     if round(len(value)/2) < 1:
         return value[0]
         
@@ -22,5 +32,5 @@ def merge_sort(value):
                 if temp[n] > temp[n+1]:
                     temp[n], temp[n+1] = temp[n + 1], temp[n]
             sorter.append(temp)      
-    print(sorter)
-    
+    print(sorter)'''
+ 
