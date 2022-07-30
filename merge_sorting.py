9264cf6395a21127a5c7b48bb9c1cd44784e513f
Emmanuel Ikpesu <emmanarutops2@gmail.com>
2022-07-30 23:20:05 +0100

    fix: Accounted for List remained

--- merge_sorting.py
+++ merge_sorting.py
@@ -1,31 +1,40 @@
 value = [3,4,5,2,1,5]
 
-
 def merge_sort(value):
+# check if lenght of list is greater than one
     if len(value) > 1:
+# split list into two equal half
         mid = round(len(value)/2)
         left_value = value[:mid]
         right_value = value[mid:]
-        
+# recusively sort each half
         merge_sort(left_value)
         merge_sort(right_value)
         
         i = 0
         j = 0
-        k = 0
-        
+        k = 0       
+# Merge both halfs
         while i < len(left_value) and j < len(right_value):
             if left_value[i] < right_value[j]:
                 value[k] = left_value[i]
                 i += 1
-                k += 1
+            
             else:
                 value[k] = right_value[j]
                 j += 1
-                k += 1
-    print(value)
-
+            k += 1           
+# Merge remainder
+        while i < len(left_value):
+            value[k] = left_value[i]
+            i += 1
+            k += 1
+        while i < len(right_value):
+            value[k] = right_value[j]
+            j += 1
+            k += 1
 merge_sort(value)
+print(value)
         
 
         
