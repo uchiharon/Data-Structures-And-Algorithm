b9c3bebea2907b99d91a09c88c5f8fd5ebebb782
Emmanuel Ikpesu <emmanarutops2@gmail.com>
2022-07-31 22:57:22 +0100

    fix: Remove Unwanted Part

--- merge_sorting.py
+++ merge_sorting.py
@@ -34,28 +34,4 @@ def merge_sort(value):
             k += 1
 merge_sort(value)
 print(value)
-        
-
-        
-'''
-
-for n in value:
-    if len(n) > 1:
-        for m in range(len(n)-1):
-            if n[m] > n[m+1]:
-                n[m+1],n[m] = n[m],n[m+1]
-                
-                
-    if round(len(value)/2) < 1:
-        return value[0]
-        
-    else:
-        for n in range(round(len(value)/2)):
-            n*=2
-            temp = value[n] + value[n+2]
-            for n in range(len(temp) - 1):
-                if temp[n] > temp[n+1]:
-                    temp[n], temp[n+1] = temp[n + 1], temp[n]
-            sorter.append(temp)      
-    print(sorter)'''
  
