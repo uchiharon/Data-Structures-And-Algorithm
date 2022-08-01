211fe09baaf5d9cbf0b76357edf575c475728185
Emmanuel Ikpesu <emmanarutops2@gmail.com>
2022-08-01 20:54:05 +0100

    fix: Seperate merge sorter and merger functions

--- merge_sorting.py
+++ merge_sorting.py
@@ -9,29 +9,36 @@ def merge_sort(value):
 # recusively sort each half
         merge_sort(left_value)
         merge_sort(right_value)
+        merge(value, left_value, right_value)
         
-        i = 0
-        j = 0
-        k = 0       
-# Merge both halfs
-        while i < len(left_value) and j < len(right_value):
-            if left_value[i] < right_value[j]:
-                value[k] = left_value[i]
-                i += 1
-            
-            else:
-                value[k] = right_value[j]
-                j += 1
-            k += 1           
-# Merge remainder
-        while i < len(left_value):
+        
+     
+# Merger
+
+def merge(value, left_value, right_value):
+    i = 0
+    j = 0
+    k = 0  
+    while i < len(left_value) and j < len(right_value):
+        if left_value[i] < right_value[j]:
             value[k] = left_value[i]
             i += 1
-            k += 1
-        while j < len(right_value):
+        
+        else:
             value[k] = right_value[j]
             j += 1
-            k += 1
+        k += 1           
+# Merge remainder
+    while i < len(left_value):
+        value[k] = left_value[i]
+        i += 1
+        k += 1
+    while j < len(right_value):
+        value[k] = right_value[j]
+        j += 1
+        k += 1
+        
+        
 merge_sort(value)
 print(value)
  
