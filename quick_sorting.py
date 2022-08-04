b2498f3ed793ac4dcf546f6ffa20cb874b98d836
Emmanuel Ikpesu <emmanarutops2@gmail.com>
2022-08-04 23:17:37 +0100

    Feat: Creat partitional and Swapper

--- quick_sorting.py
+++ quick_sorting.py
@@ -3,15 +3,23 @@ value = [3,4,5,2,1,5]
 def quick_sort(value):
     left = 0
     right = len(value) - 1
-    if 
-    print(mid)
-    i = 0
-    for n in range(mid + 1):
-        if value[i] >= value[mid]:
-            value[i], value[mid], value[mid-1] = value[mid-1], value[i], value[mid]
-            mid -= 1
-        else:
-            i += 1
+    index = partition(value, left, right)
+
+
+def partition(arr, left, right):
+    pivot = arr[round((left+right)/2)]
+    while left <= right: 
+        while arr[left] < pivot: left +=1;
+        while arr[right] > pivot: left -=1;
+        
+        if left <= right:
+            swap(arr, left, right)
+            left +=1
+            right -=1
+        
+def swap(arr, left, right):
+    arr[left], arr[right] = arr[left], arr[right]
+        
             
             
 
